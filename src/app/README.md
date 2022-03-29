9cf9e09e506cf7b4ee0dd3e691ea909f3798ac6e
Divakar <divakar.kumar@hm.com>
2022-03-29 20:56:56 +0530

    Initial commit

---
+++ src/app/README.md
@@ -0,0 +1,4 @@
+# speechtransciptor, transcript your speech and display to multiple clients
+
+This is a demo application to check the performance of WebPubSub in one to many scenarios.
+
---
+++ src/app/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { WebPubSubServiceClient } = require('@azure/web-pubsub');
+const axios = require('axios').default;
+
+const app = express();
+let connectionString = process.argv[2] || process.env.WebPubSubConnectionString;
+let endpoint = new WebPubSubServiceClient(connectionString, 'speechtransciptor');
+
+app.get('/negotiate', async (req, res) => {
+  let id = req.query.id || Math.random().toString(36).slice(2,7);
+  let roles = req.query.id
+    ? [`webpubsub.sendToGroup.${id}-control`, `webpubsub.joinLeaveGroup.${id}`]
+    : [`webpubsub.sendToGroup.${id}`, `webpubsub.joinLeaveGroup.${id}-control`];
+  let token = await endpoint.getClientAccessToken({ roles: roles });
+  res.json({
+    id: id,
+    url: token.url
+  });
+});
+
+app.get('/speech-token', async (req, res) => {
+
+  res.setHeader('Content-Type', 'application/json');
+  const speechKey = process.argv[3] || process.env.SPEECH_KEY;  
+  const speechRegion = process.argv[4] ||  process.env.SPEECH_REGION;  
+
+  const headers = {
+    headers: {
+      'Ocp-Apim-Subscription-Key': speechKey,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  try {
+    const tokenResponse = await axios.post(`https://${speechRegion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, null, headers);
+    res.send({ token: tokenResponse.data, region: speechRegion });
+  } catch (err) {
+    console.log(err);
+    res.status(401).send('There was an error authorizing your speech key.');
+  }
+
+});
+
+app.use(express.static('public'));
+app.listen(8080, () => console.log('app started'));
