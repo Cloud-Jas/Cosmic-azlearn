4f168af1af8f2c7b600d6a0876469f20afc062ba
Divakar <divakar.kumar@hm.com>
2022-03-29 22:29:07 +0530

    updated api and app project

---
+++ src/api/FxAzureMap.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Azure.Core;
+using Azure.Identity;
+using System;
+
+namespace azlearn.cosmic.API
+{
+    public class FxAzureMap
+    {
+        private readonly ILogger<FxAzureMap> _logger;
+
+        private static readonly string[] allowd = { "https://samples.azuremaps.com/",
+                                                    "https://demo.azuremaps.com/",
+                                                    "http://localhost"};
+
+        public FxAzureMap(ILogger<FxAzureMap> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("FxAzureMap")]
+        [OpenApiOperation(operationId: "GetToken", tags: new[] { "name" })]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> GetToken(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "azureMapsToken")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+           string referer = req.Headers["Referer"];
+            if (string.IsNullOrEmpty(referer))
+                return new UnauthorizedResult();
+
+            string result = Array.Find(allowd, site => referer.StartsWith(site, StringComparison.OrdinalIgnoreCase));
+            if (string.IsNullOrEmpty(result))
+                return new UnauthorizedResult();
+
+            var tokenCredential = new DefaultAzureCredential();
+            var accessToken = await tokenCredential.GetTokenAsync(
+                new TokenRequestContext(new[] { "https://atlas.microsoft.com/.default" })
+            );
+
+            return new OkObjectResult(accessToken.Token);
+        }
+    }
+}
+
