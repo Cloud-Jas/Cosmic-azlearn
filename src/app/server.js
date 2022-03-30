00189711fc101b1658e363dabab47b75446684e0
Divakar <divakar.kumar@hm.com>
2022-03-30 15:09:05 +0530

    added cors

--- src/app/server.js
+++ src/app/server.js
@@ -18,6 +18,15 @@ app.get('/negotiate', async (req, res) => {
   });
 });
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
 app.get('/speech-token', async (req, res) => {
 
   res.setHeader('Content-Type', 'application/json');
