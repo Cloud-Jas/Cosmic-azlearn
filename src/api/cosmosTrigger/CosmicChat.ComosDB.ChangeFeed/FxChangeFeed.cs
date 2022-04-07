c769de344ac8c264e12f6dc33177b7730f92679a
Divakar <divakar.kumar@hm.com>
2022-04-08 00:53:09 +0530

    updated cosmostrigger with webpubsub output binding

--- src/api/cosmosTrigger/CosmicChat.ComosDB.ChangeFeed/FxChangeFeed.cs
+++ src/api/cosmosTrigger/CosmicChat.ComosDB.ChangeFeed/FxChangeFeed.cs
@@ -1,14 +1,10 @@
-﻿using AzureFunctions.Extensions.Middleware;
-using AzureFunctions.Extensions.Middleware.Abstractions;
-using CosmicChat.Shared.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using CosmicChat.Shared.Models;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.WebPubSub;
+using Microsoft.Azure.WebPubSub.Common;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CosmicChat.CosmosDB.ChangeFeed
@@ -16,10 +12,10 @@ namespace CosmicChat.CosmosDB.ChangeFeed
    public class FxChangeFeed
    {
       private readonly ILogger<FxChangeFeed> _logger;
-      
+
       public FxChangeFeed(ILogger<FxChangeFeed> logger)
       {
-         _logger = logger;         
+         _logger = logger;
       }
       [FunctionName("CosmosDBTrigger")]
       public async Task CosmosDBTrigger([CosmosDBTrigger(
@@ -27,13 +23,24 @@ namespace CosmicChat.CosmosDB.ChangeFeed
             containerName: "CosmicUsers",
             Connection = "CosmicDBIdentity",
             LeaseContainerName = "leasesCosmicUsers",
-            CreateLeaseContainerIfNotExists =true)]IReadOnlyList<User> input)
+            CreateLeaseContainerIfNotExists =true)]IReadOnlyList<User> input, [WebPubSub(Hub = "CosmosPark")] IAsyncCollector<WebPubSubAction> operation)
       {
 
          if (input != null && input.Count > 0)
          {
             _logger.LogInformation("Documents modified " + input.Count);
             _logger.LogInformation("First document Id " + input[0].name);
+
+            await operation.AddAsync(new SendToGroupAction
+            {
+               Group = input[0].address.country.code + "-location",
+               Data = BinaryData.FromObjectAsJson(new
+               {
+                  latitude = input[0].address.position.latitude,
+                  longitude = input[0].address.position.longitude
+               }),
+               DataType = WebPubSubDataType.Json
+            });
          }
 
       }
