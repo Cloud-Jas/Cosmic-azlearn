4ccc5433de6d8ebdb4170a4bdba3d1a9dfe23c25
Divakar <divakar.kumar@hm.com>
2022-04-07 22:40:12 +0530

    updated cosmos trigger

--- src/api/cosmosTrigger/CosmicChat.ComosDB.ChangeFeed/FxChangeFeed.cs
+++ src/api/cosmosTrigger/CosmicChat.ComosDB.ChangeFeed/FxChangeFeed.cs
@@ -1,4 +1,8 @@
-﻿using CosmicChat.Shared.Models;
+﻿using AzureFunctions.Extensions.Middleware;
+using AzureFunctions.Extensions.Middleware.Abstractions;
+using CosmicChat.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,14 +11,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CosmicChat.API
+namespace CosmicChat.CosmosDB.ChangeFeed
 {
    public class FxChangeFeed
    {
-      private readonly ILogger _logger;
-      public FxChangeFeed(ILogger logger)
+      private readonly ILogger<FxChangeFeed> _logger;
+      
+      public FxChangeFeed(ILogger<FxChangeFeed> logger)
       {
-         _logger = logger;
+         _logger = logger;         
       }
       [FunctionName("CosmosDBTrigger")]
       public async Task CosmosDBTrigger([CosmosDBTrigger(
@@ -22,14 +27,15 @@ namespace CosmicChat.API
             containerName: "CosmicUsers",
             Connection = "CosmicDBIdentity",
             LeaseContainerName = "leasesCosmicUsers",
-            CreateLeaseContainerIfNotExists =true)]IReadOnlyList<User> input,
-          ILogger log)
+            CreateLeaseContainerIfNotExists =true)]IReadOnlyList<User> input)
       {
+
          if (input != null && input.Count > 0)
          {
             _logger.LogInformation("Documents modified " + input.Count);
             _logger.LogInformation("First document Id " + input[0].name);
          }
+
       }
    }
 }
